569e42afc73790f29c10e7748fe169f4f17d1444
KiichiroNakamura <47166584+KiichiroNakamura@users.noreply.github.com>
2021-09-17 13:57:05 +0900

    202109171356 WebM再生OK

--- src/components/FileList.jsx
+++ src/components/FileList.jsx
@@ -1,1 +1,19 @@
 import React from "react";
+
+export const FileList = () => {
+  return (
+    <div className="fileList">
+      list
+      <ul>
+        <div className="list-row">
+          <li>file1</li>
+          <button>再生</button>
+        </div>
+        <div className="list-row">
+          <li>file2</li>
+          <button>再生</button>
+        </div>
+      </ul>
+    </div>
+  );
+};
--- src/components/MoviePlayer.jsx
+++ src/components/MoviePlayer.jsx
@@ -5,6 +5,14 @@ import { Player, ControlBar, PlaybackRateMenuButton } from "video-react";
 export const MoviePlayer = () => {
   const [rate, setRate] = useState(1);
   const [player, setPlayer] = useState(null);
+  const [mediaSrc, setMediaSrc] = useState(
+    // "https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+    "https://dl8.webmfiles.org/big-buck-bunny_trailer.webm"
+  );
+  const [mediaType, setMediaType] = useState(
+    //   "video/mp4"
+    "video/webm"
+  );
   // setPlayer(Player);
   return (
     <React.Fragment>
@@ -12,10 +20,7 @@ export const MoviePlayer = () => {
         <ControlBar>
           <PlaybackRateMenuButton order={7} rates={[2, 1.5, 1.0, 0.5]} />
         </ControlBar>
-        <source
-          src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-          type="video/mp4"
-        />
+        <source src={mediaSrc} type={mediaType} />
       </Player>
     </React.Fragment>
   );
